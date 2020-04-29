d7b11bc4707d4d74b70773487399cef2466261d4
Vladimir Taranovskiy <8756136@gmail.com>
2020-04-29 23:38:41 +0300

    Login page added. Auth base logic implemented.

---
+++ pages/admin/login.vue
@@ -0,0 +1,86 @@
+<template>
+  <el-card shadow="always" :style="{ width: '500px' }">
+    <el-form
+      ref="form"
+      :model="controls"
+      :rules="rules"
+      @submit.prevent.native="onSubmit"
+    >
+      <h2>Войти в панель администратора</h2>
+      <el-form-item label="Логин" prop="login">
+        <el-input v-model.trim="controls.login" />
+      </el-form-item>
+
+      <el-form-item label="Пароль" prop="password">
+        <el-input v-model.trim="controls.password" type="password" />
+      </el-form-item>
+      <div class="mb-2">
+        <el-form-item>
+          <el-button
+            type="primary"
+            round
+            native-type="submit"
+            :loading="loading"
+          >
+            Войти
+          </el-button>
+        </el-form-item>
+      </div>
+    </el-form>
+  </el-card>
+</template>
+
+<script>
+export default {
+  layout: 'empty',
+  data() {
+    return {
+      loading: false,
+      controls: {
+        login: '',
+        password: '',
+      },
+      rules: {
+        login: [
+          {
+            required: true,
+            message: 'Введите логин',
+            trigger: 'blur',
+          },
+        ],
+        password: [
+          {
+            required: true,
+            message: 'Введите пароль',
+            trigger: 'blur',
+          },
+          {
+            min: 6,
+            message: 'Пароль должен содержать не менее 6 символов',
+            trigger: 'blur',
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$refs.form.validate(async (valid) => {
+        if (valid) {
+          this.loading = true;
+          try {
+            const formData = {
+              login: this.controls.login,
+              password: this.controls.password,
+            };
+            await this.$store.dispatch('auth/login', formData);
+            this.$router.push('/admin');
+          } catch (error) {
+            this.loading = false;
+          }
+        }
+      });
+    },
+  },
+};
+</script>
