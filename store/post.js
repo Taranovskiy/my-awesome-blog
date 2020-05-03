d86932e3f3541faed42d5dbd3e0ff382f4e3e8a9
Vladimir Taranovskiy <8756136@gmail.com>
2020-05-03 20:25:51 +0300

    Update post admin page added

--- store/post.js
+++ store/post.js
@@ -1,25 +1,35 @@
+const posts = [
+  {
+    title: 'Post 1',
+    date: new Date(),
+    views: 22,
+    comments: [1, 2],
+    _id: 'id1',
+  },
+  {
+    title: 'Post 2',
+    date: new Date(),
+    views: 22,
+    comments: [1, 2],
+    _id: 'id2',
+  },
+];
+
 export const actions = {
   async fetchAdmin(ctx) {
     return await new Promise((resolve) => {
       setTimeout(() => {
-        resolve([
-          {
-            title: 'Post 1',
-            date: new Date(),
-            views: 22,
-            comments: [1, 2],
-            _id: Math.random(),
-          },
-          {
-            title: 'Post 2',
-            date: new Date(),
-            views: 22,
-            comments: [1, 2],
-            _id: Math.random(),
-          },
-        ]);
+        resolve(posts);
       }, 1000);
     });
   },
   async remove(ctx, id) {},
+  async update(ctx, { id, text }) {},
+  async fetchAdminById(ctx, id) {
+    return await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(posts.find((item) => item._id === id));
+      }, 1000);
+    });
+  },
 };
